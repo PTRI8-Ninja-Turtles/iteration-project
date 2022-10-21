a9f810df4234f0e067991db628e5419ef7d9d175
Richter Lau <ar.rick@hotmail.com>
2022-10-20 22:20:00 -0400

    Yarden (#21)
    * Frontend Fixes
    
    * grid creation
    
    * navbar fix, room exit btn, settings bar vh fix
    
    * post-it card style update
    
    * room names populating main page
    
    * room name populates
    
    Co-authored-by: yardenedry <yardenedry0@gmail.com>

--- client/components/ActiveRooms.jsx
+++ client/components/ActiveRooms.jsx
@@ -1,12 +1,31 @@
 import React, { useEffect, useState } from 'react';
 
 const ActiveRooms = () => {
+  const [users, setUsers] = useState([]);
   const [rooms, setRooms] = useState([]);
   const fetchRooms = async () => {
     // GET request to server api endpoint with subject in params
     const roomData = await fetch('/api/rooms/allRooms').then(response => response.json());
     if(Array.isArray(roomData)) setRooms(roomData);
     else setRooms([]);
+
+
+    let [ temp, users ] = '';
+    const usersArr = [];
+
+    for (let i = 0; i < roomData.length; i++){
+      // console.log(roomData[i]);
+      temp = roomData[i];
+      users = temp['allowedUsers'];
+      usersArr.push(...users);
+    }
+  
+    const noDupesUsersArr = [...new Set(usersArr)];
+    if (noDupesUsersArr[0] === 'undefined') noDupesUsersArr.shift();
+
+    setUsers(noDupesUsersArr);
+    
+
   };
 
   useEffect(() => {
@@ -28,7 +47,9 @@ const ActiveRooms = () => {
         {rooms.map((rooms, i) => (
           <div className='grid-item' key={i}>
             <div className='grid-item-front'>
-              <p>Room {i + 1}</p>
+              <p id='active-room-name'>{users[i]}'s Room</p>
+              {/* <p>Room {i + 1}</p> */}
+
               {/* we want to add description of room but make the font smaller */}
               {/* <p>{rooms.host.nickname}&apos;s {rooms.subject} Room</p> */}
 
